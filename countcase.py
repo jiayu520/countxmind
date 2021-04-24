22ca54de7f9c20514472f2c431defba9bbe4dede
jia <jiayu520000@outlook.com>
2021-04-24 16:44:57 +0800

    添加成功率

--- countcase.py
+++ countcase.py
@@ -1,5 +1,5 @@
 #-*- coding: UTF-8 -*-
-
+from __future__ import division
 from xmindparser import xmind_to_dict
 
 class CountXmind():
@@ -7,6 +7,7 @@ class CountXmind():
         self.total = 0
         self.success = 0
         self.failed = 0
+        self.execd = 0
 
         self.filepath = filepath
 
@@ -15,16 +16,22 @@ class CountXmind():
         #print(self.xmind_dict)
         self.xmind_list = self.xmind_dict[0]['topic']['topics']
         #print(self.xmind_list)
+        #print len(self.xmind_list)
+        # for i in range(len(self.xmind_list)):
+        #     return self.xmind_list[i]['title']
         self.count(self.xmind_list)
-        return self.total,self.success,self.failed
+        return self.total,self.execd,self.success,self.failed,self.modelu
 
     def count(self,case):
+        self.modelu = []
         for i in range(len(case)):
+            self.modelu.append(case[i]['title'])
             if case[i].__contains__('topics'):#带topics标签意味挣有子标题，递归执行方法
                 self.count(case[i]['topics'])
+                #print case[i]['topics']['title']
             else:
                 if case[i].__contains__('makers'):
-
+                    self.execd += 1
                     if case[i]['makers'].__contains__('task-done'):
                         self.success += 1
                     elif case[i]['makers'].__contains__('symbol-wrong'):
@@ -35,8 +42,10 @@ class CountXmind():
 
 
 if __name__ == '__main__':
-    r = CountXmind('//Users/jia/Documents/VR带看二期优化测试点的副本.xmind')
+    r = CountXmind('/Users/jia/Downloads/jiayu.xmind')
     result = r.covert()
     print ('用例总数为 %s' % result[0])
-    print ('执行成功用例数 %s' % result[1])
-    print ('执行失败用例数 %s' % result[2])
+    print ('已执行用例数 %s' % result[1])
+    print ('执行成功用例数 %s, 成功率为%1.f%%' % (result[2],(result[2]/result[1]*100)))
+    print ('执行失败用例数 %s' % result[3])
+    print ('模块 %s' % result[4])
